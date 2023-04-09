8a37ddb418b7b7620293f0d0e716bbb484da2c93
ivan1810 <78267912+ivan1810@users.noreply.github.com>
2023-04-09 22:16:08 +0300

    add transaction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,15 +7,18 @@ import data from '../utils/data.json';
 import { FriendList } from './Friends/FriendList';
 import friends from '../utils/friends.json';
 
+import { TransactionHistory } from './Transaction/Transaction';
+import transactions from '../utils/transactions.json';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         gap: 60,
+        padding: 50,
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
@@ -31,6 +34,7 @@ export const App = () => {
       />
       <Statistics title={'Upload stats'} stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
